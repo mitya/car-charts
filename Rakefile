214f2c1ba4af2a461afa01d3646385b2d6dbeaac
Dmitry Sokurenko <dmitry@sokurenko.name>
2012-06-05 12:43:15 +0400

    first

---
+++ Rakefile
@@ -0,0 +1,10 @@
+$:.unshift("/Library/RubyMotion/lib")
+require 'motion/project'
+
+Motion::Project::App.setup do |app|
+  # Use `rake config' to see complete project settings.
+  app.name = 'Hello'
+  # app.deployment_target = "5.0"
+  # app.frameworks += ['AVFoundation']
+  # app.device_family = [:ipad, :iphone]
+end
---
+++ app/app_delegate.rb
@@ -0,0 +1,43 @@
+class AppDelegate
+  attr_accessor :window, :navigationController
+  
+  def application(application, didFinishLaunchingWithOptions:launchOptions)
+    self.navigationController = UINavigationController.alloc.initWithRootViewController(MainViewController.alloc.init)
+    navigationController.delegate = self
+
+    self.window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
+    window.backgroundColor = UIColor.whiteColor
+    window.rootViewController = navigationController
+    window.makeKeyAndVisible
+    
+    return true
+  end
+end
+
+class MainViewController < UITableViewController
+  attr_accessor :data
+  
+  def viewDidLoad
+    super
+    self.title = "Главное окно"
+    
+    dataPath = NSBundle.mainBundle.pathForResource("ya-mods-parsed-bin", ofType:"plist")
+    self.data = NSMutableArray.alloc.initWithContentsOfFile(dataPath)
+  end
+  
+  def tableView(tv, numberOfRowsInSection:section)
+    return data.count
+  end
+  
+  def tableView(tv, cellForRowAtIndexPath:ip)  
+    unless cell = tv.dequeueReusableCellWithIdentifier("cell")
+      cell = UITableViewCell.alloc.initWithStyle(UITableViewCellStyleDefault, reuseIdentifier:"cell")
+      cell.selectionStyle = UITableViewCellSelectionStyleNone
+      cell.textLabel.adjustsFontSizeToFitWidth = true
+    end
+    
+    item = data[ip.row]        
+    cell.textLabel.text = item['key']
+    return cell
+  end
+end
